f47828ed1ffb0ac5a8b1a3c841ebf54c5c8f1de5
artissceC <artis.2004.07@gmail.comC>
2024-05-25 22:34:35 -0600

    intento de cambio en background

--- proyecto/src/components/admin/MenuAdmin.jsx
+++ proyecto/src/components/admin/MenuAdmin.jsx
@@ -8,7 +8,7 @@ const MenuAdmin = () => {
         <Container>
             <div className="row justify-content-center">
                 <div className="col-12 col-md-10 col-lg-8">
-                        <h2 className="text-center mb-4">Admin Menu</h2>
+                        <h2 className="text-center mb-4">Menu principal</h2>
                         <table className="menu-table table table-striped">
                             <tbody>
                                 <tr>
